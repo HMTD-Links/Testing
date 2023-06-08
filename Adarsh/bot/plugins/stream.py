a454822f9dbe82fdf8492ade23bb88dbcef206f3
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-06-08 07:13:24 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -66,7 +66,7 @@ async def private_receive_handler(c: Client, m: Message):
         )
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-        file_name = get_media_file_name(m)
+        file_name = get_name(log_msg)
         file_hash = get_hash(log_msg)
         watch_link = f"{Var.URL}Watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         stream_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
@@ -75,11 +75,11 @@ async def private_receive_handler(c: Client, m: Message):
         shortened_stream_link = await get_shortlink(stream_link)
         shortened_watch_link = await get_shortlink(watch_link)
         
-        msg_text ="""<b>Your Link is Generated... ⚡\n\n📁 File Name :- {}\n\n📦 File Size :- {}\n\n🔠 File Captain :- {}\n\n📥 Fast Download Link :- {}\n\n🖥 Watch Link :- {}\n\n❗ Note :- This Link is Permanent and Won't Gets Expired 🚫\n\n©️ <a href=https://t.me/Star_Bots_Tamil><b></b>Star Bots Tamil</a></b></b>"""
+        msg_text ="""<b>Your Link is Generated... ⚡\n\n📁 File Name :- {file_name}\n\n📦 File Size :- {file_size}\n\n🔠 File Captain :- {file_caption}\n\n📥 Fast Download Link :- {}\n\n🖥 Watch Link :- {}\n\n❗ Note :- This Link is Permanent and Won't Gets Expired 🚫\n\n©️ <a href=https://t.me/Star_Bots_Tamil><b></b>Star Bots Tamil</a></b></b>"""
 
         await log_msg.reply_text(text=f"<b>Request By :- <a href='tg://user?id={m.from_user.id}'>{m.from_user.first_name}</a>\nID :- <code>{m.from_user.id}</code>\n📥 Download Link :- {stream_link}</b>", disable_web_page_preview=True, parse_mode=ParseMode.HTML, quote=True)
         await m.reply_text(
-            text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), stream_link, watch_link),
+            text=msg_text.format(stream_link, watch_link),
             quote=True,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📥 Fast Download Link", url=stream_link), #Download Link
